b4b281f90f31ad37213f7912dc68e2a614c240e7
Rohan Munot <rohanmunot24@gmail.com>
2025-12-24 15:24:45 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/src/components/theme/theme-provider";
+
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Mr.Manager",
+  description: "Provider Web App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={outfit.variable} suppressHydrationWarning>
+      <body className={`${outfit.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="bg-background min-h-screen">Hello World</div>;
+}
